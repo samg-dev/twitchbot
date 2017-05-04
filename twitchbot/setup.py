048ad3b9bb42f7f4c4a44621a4709e8e799b45ac
Samuel Garnham <samuelgarnham@100.local>
2017-05-04 11:40:29 +0200

    Config file containing information for running bot, sensitive information has dummy values Utility file containing functions for usage Main bot file

---
+++ twitchbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="twitchbot",
+      version="0.1",
+      description="Customizable twitch bot",
+      url="http://github.com/samuelgarnham/twitchbot",
+      author='samuelgarnham',
+      author_email="1120118@my.brunel.ac.uk",
+      license='MIT',
+      packages=['twitchbot'],
+      zip_safe=False)
