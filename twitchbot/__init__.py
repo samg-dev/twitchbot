7055f06075c982f322b1c6a9944c56c63c8c3bff
Samuel Garnham <samuelgarnham@100.local>
2017-05-04 16:27:57 +0200

    Moved directory structure around

---
+++ twitchbot/__init__.py
@@ -0,0 +1,3 @@
+from cfg import *
+from utils import *
+from bot import *
---
+++ twitchbot/cfg.py
@@ -0,0 +1,16 @@
+# cfg.py
+# This contains the configuration information for the bot to function
+
+# General stuff
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+USERNAME = "pondracbot"
+CHANNEL = "moofle"
+RATE = (20/30) # Messages per second
+oplist = {}
+# Secret stuff
+
+# Retrieve here after logging in to bot account: https://www.twitchtools.com/chat-token
+PASS = ""
+
